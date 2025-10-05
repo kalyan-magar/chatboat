780e41bbd463c1c80c8afc1c9b0e545268ad8a58
kalyan-Infoprosys <kalyan.magar@infoprosys.co.in>
2025-10-05 16:52:09 +0530

    changed a to link

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import CheckoutPage from './pages/CheckoutPage'
 import HomePage from './pages/HomePage'
 import { Routes,Route } from 'react-router'
 import OrdersPage from './pages/OrdersPage'
+import TrackingPage from './pages/TrackingPage'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Route index element={<HomePage/>} />
        <Route path='checkout' element={<CheckoutPage/>} />
        <Route path='orders' element={<OrdersPage />} />
+         <Route path='tracking' element={<TrackingPage />} />
 
     </Routes>
     
--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,39 +1,10 @@
 import React from 'react'
 import "./orders.css"
+import Header from '../components/Header'
 const OrdersPage = () => {
   return (
     <>
-      <div className="header">
-      <div className="left-section">
-        <a href="/" className="header-link">
-          <img className="logo"
-            src="images/logo-white.png" />
-          <img className="mobile-logo"
-            src="images/mobile-logo-white.png" />
-        </a>
-      </div>
-
-      <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
-
-        <button className="search-button">
-          <img className="search-icon" src="images/icons/search-icon.png" />
-        </button>
-      </div>
-
-      <div className="right-section">
-        <a className="orders-link header-link" href="orders.html">
-
-          <span className="orders-text">Orders</span>
-        </a>
-
-        <a className="cart-link header-link" href="/checkout">
-          <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
-          <div className="cart-text">Cart</div>
-        </a>
-      </div>
-    </div>
+  <Header/>
 
     <div className="orders-page">
       <div className="page-title">Your Orders</div>
@@ -81,7 +52,7 @@ const OrdersPage = () => {
             </div>
 
             <div className="product-actions">
-              <a href="tracking.html">
+              <a href="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
@@ -109,7 +80,7 @@ const OrdersPage = () => {
             </div>
 
             <div className="product-actions">
-              <a href="tracking.html">
+              <a href="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
@@ -160,7 +131,7 @@ const OrdersPage = () => {
             </div>
 
             <div className="product-actions">
-              <a href="tracking.html">
+              <a href="/tracking">
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
