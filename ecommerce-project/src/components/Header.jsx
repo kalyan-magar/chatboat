99a7c41747ee17822762459b9debfd2292b4c619
kalyan-Infoprosys <kalyan.magar@infoprosys.co.in>
2025-10-10 01:36:12 +0530

    get cart from backend and display quantity

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './header.css'   
 import { Link } from 'react-router'
 
-const Header = () => {
+const Header = ({cart}) => {
+  let totalQuantity =0;
+  cart.forEach((cartItem)=>{
+   totalQuantity += cartItem.quantity;
+  })
+
   return (
     <>
      <div className="header">
@@ -31,7 +36,7 @@ const Header = () => {
 
         <Link className="cart-link header-link" to="/checkout">
           <img className="cart-icon" src="images/icons/cart-icon.png" />
-          <div className="cart-quantity">3</div>
+          <div className="cart-quantity">{totalQuantity}</div>
           <div className="cart-text">Cart</div>
         </Link>
       </div>
